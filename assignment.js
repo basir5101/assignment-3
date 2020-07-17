6cf8470d0f1fe71bfb9198ebaecf5e2d5fbb781a
basir5101 <67514986+basir5101@users.noreply.github.com>
2020-07-17 18:47:29 +0600

    assignment complete

--- assignment.js
+++ assignment.js
@@ -1,19 +1,24 @@
-//Feet To Mile Converter
-// Formula : divide feet value by 5280;
-
+// 1. Feet To Mile Converter
 
 function feetToMile (feet){
-    if ( feet == 0 ){
-        return "0 feet is equal to 0 mile";
+    if ( feet < 0 ){
+        return "Distance can't be negative value";
+    }
+    else if(feet == 0){
+        return 0;
     }
     else{
-        var mile = feet / 5280;
+        const mile = feet / 5280;
         return mile;
     }
 };
 
 
- // Wood Calculator
+
+
+
+ //2.  Wood Calculation
+
 function woodCalculator(chair, table, bed){
     if(chair < 0 || table <0 || bed < 0){
         return "Number of wood can't be negative value"
@@ -25,46 +30,51 @@ function woodCalculator(chair, table, bed){
         var totalWood = woodForChair + woodForTable + woodForBed;
         return totalWood;
     }
-}
+};
 
 
  
 
-//Brick Calculation
+//3. Bricks Calculation
 
 function brickCalculator(floor){
     if(floor <= 0){
-        return "number of floor can't be negative or zero"
+        return "Sorry! Number of floor can't be negative or zero";
     }
-    else if (floor <= 10){
-        var brick = floor * 15 * 1000;
-        return brick;
+    else if(floor <= 10){
+        var bricksForTenFloor = floor * ( 15 * 1000);
+        return bricksForTenFloor;
     }
-    else if (floor <= 20){
-        var brick = floor * 12 * 1000;
-        return brick;
+    else if(floor > 10 && floor <=20 ){
+        var firstTenFloor = 10;
+        var afterTenFloor = floor - firstTenFloor;
+        var bricksForTwentyFloor = (firstTenFloor * (15 * 1000) + afterTenFloor * (12 * 1000));
+        return bricksForTwentyFloor;
+    }
+    else {
+        AfterTwentyFloor = floor - 20;
+        twentyFloorBricks = (10 * (15 * 1000) + 10 * (12 * 1000));
+        totalBricks = (AfterTwentyFloor * (10 * 1000) + twentyFloorBricks);
+        return totalBricks;
+    }
+};
+
+//4. Finding Tiny Friend
+
+
+function tinyFriend(friends){
+    var tinyFriendName = friends[0];
+    if(friends.length == 0){
+        return "Please, Add your friend Name";
     }
     else{
-        var brick = floor * 10 * 1000;
-        return brick;
-    }
-}
-
-
-
-// Finding Tiny Friend
-
-
-function tinyFriend(friendsName){
-    var tinyFriendNameLength = friendsName[0].length;
-
-    for(var i = 0; i < friendsName.length; i++){
-
-        var currentNameLength = friendsName[i].length;
-
-            if(currentNameLength < tinyFriendNameLength){
-                tinyFriendNameLength = currentNameLength
+        for(var i = 0; i < friends.length; i++){
+            var element = friends[i];
+            if(element.length <= tinyFriendName.length){
+                tinyFriendName = element;   
             }
+        }
     }
-    return tinyFriendNameLength;
-}
+    return tinyFriendName;
+};
+console.log(tinyFriend(['b', 'ba']))
